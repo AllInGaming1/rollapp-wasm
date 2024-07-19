70d2c2b61425d3cfe0a6325b856e407dc48b66ff
Shbl <142305968+shbl1201@users.noreply.github.com>
2024-07-19 18:02:58 +0530

    Updated contract with cwerrors module example

--- contracts/callback-test/src/state.rs
+++ contracts/callback-test/src/state.rs
@@ -10,4 +10,12 @@ pub struct State {
     pub owner: Addr,
 }
 
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
+pub struct CallbackError {
+    pub message: String,
+    pub code: u32,
+    pub module_name: String,
+}
+
 pub const STATE: Item<State> = Item::new("state");
+pub const CALLBACK_ERROR: Item<Option<CallbackError>> = Item::new("callback_error");
